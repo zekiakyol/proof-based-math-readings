330ddae31552a43c7fe8ce8ab709f925ce9d1196
zekiakyol <40212849+zekiakyol@users.noreply.github.com>
2024-10-06 11:32:10 +0300

    automatize rendering and allow "all" input :rocket:

--- source/render.sh
+++ source/render.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-echo "Please list the Quarto files (filename.qmd) you want to render:"
+
+MAGENTA=$(tput setaf 5)
+RESET=$(tput sgr0)
+
+printf "Please list the Quarto files you want to render. Type 'all' to render all Quarto files.\n Examples: \n\t ${MAGENTA}filename1.qmd \n\t filename1.qmd filename2.qmd \n\t all\n${RESET}"
 read quartofiles
-quarto render $quartofiles --to pdf
+
+if [ "$quartofiles" == "all" ]; then
+    quarto render *.qmd --to pdf
+else
+    quarto render $quartofiles --to pdf
+fi
+
 mv *.pdf ..
